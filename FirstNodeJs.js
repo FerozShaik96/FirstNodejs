566940e51e8d2a22b225c23a5251722e30998b14
FerozShaik96 <ferozshaik1997@gmail.com>
2024-01-05 23:11:49 +0530

    Adding Express to the Project

--- FirstNodeJs.js
+++ FirstNodeJs.js
@@ -1,4 +1,15 @@
-const http = require("http");
-const handler = require("./Routes");
-const server = http.createServer(handler);
-server.listen(3000);
+// const http = require("http");
+// const handler = require("./Routes");
+const express = require("express");
+const app = express();
+// const server = http.createServer(app);
+// server.listen(3000);
+app.use((req, res, next) => {
+  console.log("Hello");
+  next();
+});
+app.use((req, res, next) => {
+  res.send("<h1>Hello from Express </h1>");
+});
+// To Crate a server using Express
+app.listen(3000);
