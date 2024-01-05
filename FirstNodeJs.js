39bdd2b00c3dc60c7f4e95286d083d6a4f604cda
FerozShaik96 <ferozshaik1997@gmail.com>
2024-01-05 23:42:59 +0530

    Added Body Parser and limited middlewares only to  post request

--- FirstNodeJs.js
+++ FirstNodeJs.js
@@ -2,13 +2,24 @@
 // const handler = require("./Routes");
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
 // const server = http.createServer(app);
 // server.listen(3000);
-app.use((req, res, next) => {
-  console.log("Hello");
-  next();
+// app.use((req, res, next) => {
+//   console.log("Hello");
+//   next();
+// });
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    "<html><body><form action='/product' method='POST'><input type='text' name='tittle'/><input type='number' name='size'/><button type='submit'>submit</button></form></body></html>"
+  );
 });
-app.use((req, res, next) => {
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+app.use("/", (req, res, next) => {
   res.send("<h1>Hello from Express </h1>");
 });
 // To Crate a server using Express
