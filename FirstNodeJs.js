2692943d2ee09463e63ab38ffc86ed3cfc5bed01
FerozShaik96 <ferozshaik1997@gmail.com>
2024-02-28 18:05:10 +0530

    Serving html pages through route files

--- FirstNodeJs.js
+++ FirstNodeJs.js
@@ -3,6 +3,9 @@
 const express = require("express");
 const adminRoute = require("./routes/admin");
 const shopRoute = require("./routes/shop");
+const contactRoute = require("./routes/contact");
+const successfulRoute = require("./routes/successful");
+const path = require("path");
 const app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -12,10 +15,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 //   console.log("Hello");
 //   next();
 // });
+app.use("/successful", successfulRoute);
+app.use("/contact", contactRoute);
 app.use("/admin", adminRoute);
-app.use("/shop", shopRoute);
+app.use(shopRoute);
+
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page Not Found</h1>");
+  res.status(404).sendFile(path.join(__dirname, "views", "Error.html"));
 });
 // To Crate a server using Express
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,11 @@
 const express = require("express");
+const path = require("path");
 const router = express.Router();
 router.get("/add-product", (req, res, next) => {
-  res.send(
-    "<html><body><form action='/admin/add-product' method='POST'><input type='text' name='tittle'/><input type='number' name='size'/><button type='submit'>submit</button></form></body></html>"
-  );
+  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
 });
 router.post("/add-product", (req, res, next) => {
   console.log(req.body);
-  res.redirect("/shop");
+  res.redirect("/");
 });
 module.exports = router;
